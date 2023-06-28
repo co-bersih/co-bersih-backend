fe130558441db257866febdfdd7aebb863e5221c
Veivel <25278964+Veivel@users.noreply.github.com>
2023-06-28 09:57:27 +0700

    feat: secret key

--- cobersih/settings.py
+++ cobersih/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import environ
+import django.core.management.utils
 
 # Env
 env = environ.Env()
@@ -26,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env("SECRET_KEY")
+SECRET_KEY = django.core.management.utils.get_random_secret_key()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DEBUG")
