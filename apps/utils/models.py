978fa271dccbdc2cf54c754d05e3ec30fc71f409
Lyzander Marciano Andrylie <lyzanderandrylie@gmail.com>
2023-06-30 14:12:03 +0700

    Implement base model

---
+++ apps/utils/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+
+# Create your models here.
+class AppQuerySet(models.QuerySet):
+    def delete(self):
+        self.update(is_deleted=True)
+
+
+class AppManager(models.Manager):
+    def get_queryset(self):
+        return AppQuerySet(self.model, using=self._db).exclude(is_deleted=True)
+
+
+class BaseModel(models.Model):
+    class Meta:
+        abstract = True
+
+    objects = AppManager()
+    is_deleted = models.BooleanField(default=False)
+
+    def delete(self):
+        """
+        Soft delete
+        """
+        self.is_deleted = True
+        self.save()
